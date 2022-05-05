b2effd1e94e0d550a866a4d94d71b254da6c099f
deepakdalal152 <40324333+deepakdalal152@users.noreply.github.com>
2022-05-05 22:52:39 +0530

    ds

---
+++ test.js
@@ -0,0 +1,8 @@
+function changeToMumbai()
+{
+    console.log(document.getElementById("test").innerHTML = "Mumbai")
+}
+function changeToDelhi()
+{
+    console.log(document.getElementById("test").textContent = "Delhi")
+}
